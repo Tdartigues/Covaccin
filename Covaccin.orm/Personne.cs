7961587cb01a6438a35b3a10dc8bba286d267f32
Zayed Charef <zayed.charef@gmail.com>
2020-12-15 16:40:48 +0100

    feat(person-class): added

---
+++ Covaccin.orm/Personne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Covaccin.orm
+{
+    public class Personne
+    {
+        int id { get; set; }
+
+        [MaxLength(20)]
+        string nom { get; set; }
+
+        [MaxLength(20)]
+        string prenom { get; set; }
+
+        bool sexe { get; set; }
+
+        DateTime? dateNaissance { get; set; }
+
+        bool estResident_pasPersonnel { get; set; }
+
+        public Personne()
+        {
+        }
+    }
+}
