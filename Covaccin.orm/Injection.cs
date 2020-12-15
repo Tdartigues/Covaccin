bad25ac38913b0fb1c89659c03ff1d72e87efe0d
Zayed Charef <zayed.charef@gmail.com>
2020-12-15 17:01:37 +0100

    feat(injection-class)

---
+++ Covaccin.orm/Injection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Covaccin.orm
+{
+    public class Injection
+    {
+        int Id { get; set; }
+
+        [Required]
+        int IdPersonne { get; set; }
+
+        [Required]
+        int IdType { get; set; }
+
+        [MaxLength(20)]
+        public string Prenom { get; set; }
+
+        [MaxLength(20)]
+        public string Marque { get; set; }
+
+        public int IdLot { get; set; }
+
+        public Personne personne { get; set; }
+
+        public Type type { get; set; }
+    }
+}
