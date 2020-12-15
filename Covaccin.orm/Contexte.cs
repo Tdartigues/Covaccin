0a54f3ccd936961c322c0e85c4dac49bd4fc45d4
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2020-12-15 17:00:28 +0100

    Ajout de Context et TypeVaccin

---
+++ Covaccin.orm/Contexte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Covaccin.orm
+{
+    public class Contexte : DbContext
+    {
+        public DbSet<Personne> Personnes { get; set; }
+
+        public DbSet<Injection> Injections { get; set; }
+
+        public DbSet<TypeVaccin> TypeVaccins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseSqlite("Data Source = covaccin.db");
+        }
+    }
+}
