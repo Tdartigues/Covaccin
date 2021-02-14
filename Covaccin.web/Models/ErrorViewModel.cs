5fddc413bd51e2d88db1d9beb7f28f40a0355b39
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-02-14 15:38:18 +0100

    Add .wep

---
+++ Covaccin.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Covaccin.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
